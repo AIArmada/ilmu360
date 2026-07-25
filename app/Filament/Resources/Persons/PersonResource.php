bed9a034fbd06ff603c7c20630172c8483727376
Saiffil Fariz <1634949+sairiz@users.noreply.github.com>
2026-07-25 18:08:21 +0800

    refactor: extend PersonResource from filament-persons package

--- app/Filament/Resources/Persons/PersonResource.php
+++ app/Filament/Resources/Persons/PersonResource.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Persons;
 
+use AIArmada\FilamentPersons\Resources\PersonResource as PackagePersonResource;
 use App\Filament\RelationManagers\AuditsRelationManager;
-use App\Filament\Resources\Persons\Pages\CreatePerson;
-use App\Filament\Resources\Persons\Pages\EditPerson;
-use App\Filament\Resources\Persons\Pages\ListPersons;
-use App\Filament\Resources\Persons\Pages\ViewPerson;
 use App\Filament\Resources\Persons\RelationManagers\EventsRelationManager;
 use App\Filament\Resources\Persons\RelationManagers\FollowersRelationManager;
 use App\Filament\Resources\Persons\RelationManagers\MemberInvitationsRelationManager;
@@ -16,22 +13,12 @@ use App\Filament\Resources\Persons\RelationManagers\MembersRelationManager;
 use App\Filament\Resources\Persons\Schemas\PersonForm;
 use App\Filament\Resources\Persons\Tables\PersonsTable;
 use App\Models\Person;
-use BackedEnum;
-use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
-use UnitEnum;
 
-class PersonResource extends Resource
+class PersonResource extends PackagePersonResource
 {
     protected static ?string $model = Person::class;
-
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedMicrophone;
-
-    protected static ?string $recordTitleAttribute = 'name';
-
-    protected static string|UnitEnum|null $navigationGroup = 'Directory';
 
     #[\Override]
     public static function form(Schema $schema): Schema
@@ -49,27 +36,13 @@ class PersonResource extends Resource
     public static function getRelations(): array
     {
         return [
+            ...parent::getRelations(),
             MembersRelationManager::class,
             MemberInvitationsRelationManager::class,
             FollowersRelationManager::class,
             RelationManagers\InstitutionsRelationManager::class,
-            RelationManagers\TitlesRelationManager::class,
-            RelationManagers\CredentialAssignmentsRelationManager::class,
-            RelationManagers\AffiliationsRelationManager::class,
-            RelationManagers\NamesRelationManager::class,
             EventsRelationManager::class,
             AuditsRelationManager::class,
-        ];
-    }
-
-    #[\Override]
-    public static function getPages(): array
-    {
-        return [
-            'index' => ListPersons::route('/'),
-            'create' => CreatePerson::route('/create'),
-            'view' => ViewPerson::route('/{record}'),
-            'edit' => EditPerson::route('/{record}/edit'),
         ];
     }
 }
